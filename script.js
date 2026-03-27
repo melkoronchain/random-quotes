6faba5f007b1c471308270ef1622e6981493549a
melkoronchain <kamushkin89@gmail.com>
2026-03-27 21:14:30 +0300

    modified quotes array of strings to array of objects

--- script.js
+++ script.js
@@ -5,10 +5,54 @@ const quotes = [
     text: "Programs must be written for people to read.",
     author: "Harold Abelson",
   },
-  { text: "Let's fucking go!", author: "Melkor" },
   {
-    text: "Learning is light and ignorance is darkness",
+    text: "Learning is light and ignorance is darkness.",
     author: "Alexander Suvorov",
+  },
+  {
+    text: "Code is like humor. When you have to explain it, it’s bad.",
+    author: "Cory House",
+  },
+  { text: "Fix the cause, not the symptom.", author: "Steve Maguire" },
+  { text: "Simplicity is the soul of efficiency.", author: "Austin Freeman" },
+  {
+    text: "Before software can be reusable it first has to be usable.",
+    author: "Ralph Johnson",
+  },
+  { text: "Make it work, make it right, make it fast.", author: "Kent Beck" },
+  {
+    text: "First, solve the problem. Then, write the code.",
+    author: "John Johnson",
+  },
+  {
+    text: "Experience is the name everyone gives to their mistakes.",
+    author: "Oscar Wilde",
+  },
+  {
+    text: "In order to be irreplaceable, one must always be different.",
+    author: "Coco Chanel",
+  },
+  { text: "Knowledge is power.", author: "Francis Bacon" },
+  {
+    text: "The best way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
+  },
+  { text: "Stay focused and never give up.", author: "Unknown" },
+  {
+    text: "Great things are done by a series of small things brought together.",
+    author: "Vincent van Gogh",
+  },
+  {
+    text: "Do one thing every day that scares you.",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    text: "Success is not final, failure is not fatal.",
+    author: "Winston Churchill",
+  },
+  {
+    text: "Action is the foundational key to all success.",
+    author: "Pablo Picasso",
   },
 ];
 
@@ -19,8 +63,9 @@ const generateBtn = document.getElementById("generateBtn");
 function randomQuotes() {
   const index = Math.floor(Math.random() * quotes.length);
   const randomQuote = quotes[index];
-  authorElement.textContent = "— " + randomQuote.author;
-  quoteElement.textContent = randomQuote.text;
+  const { text, author } = randomQuote;
+  authorElement.textContent = "— " + author;
+  quoteElement.textContent = `"${text}"`;
 }
 
 generateBtn.addEventListener("click", randomQuotes);
